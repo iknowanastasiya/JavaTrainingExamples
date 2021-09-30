fa1fb51f7f8ebaab4e1775209a7cb5ef064ed942
AReeves8 <areeves@skillstorm.com>
2021-09-29 22:21:22 -0500

    Create chinookPracticeProblems.sql

---
+++ sql-examples/chinookPracticeProblems.sql
@@ -0,0 +1,75 @@
+
+/*
+ONE
+Provide a query showing Customers (just their full names, customer ID and country) who are not in the United States.
+*/
+
+
+/*
+TWO
+Provide a query showing the Invoices of customers who are from Brazil. 
+The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+*/
+
+
+/*
+THREE
+Provide a query showing a unique list of billing countries from the Invoice table.
+*/
+
+
+/*
+FOUR
+Provide a query that shows the invoices associated with each sales agent. 
+The resultant table should include the Sales Agent's full name and any other relevant data.
+*/
+
+
+/*
+FIVE
+How many Invoices were there in 2009 and 2013 and what are their respective sales totals?
+*/
+
+
+/*
+SIX
+Looking at the InvoiceLine table, provide a query that countss the number of line items for each Invoice.
+*/
+
+
+/*
+SEVEN
+Provide a query that includes the track name with each invoice line item.
+*/
+ 
+ 
+ /*
+ EIGHT
+ Provide a query that shows all the Tracks, but displays no IDs. 
+ The resultant table should include the Album name, Media type and Genre.
+ */
+ 
+ 
+ /*
+ NINE
+ Which sales agent made the most sales in 2010?
+ */
+
+
+/*
+TEN
+Provide a query that shows the number of customers assigned to each sales agent.
+*/
+
+
+/*
+ELEVEN
+Provide a query that shows the top 5 most purchased tracks over all.
+*/
+
+
+/*
+TWELVE
+Provide a query that shows the top 3 best selling artists.
+*/
+
